ec72cede23b4454f9d7b7efe7e6a48f409d64628
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-11 14:04:23 -0300

    Update script.js

--- JAVASCRIPT/aula16/script.js
+++ JAVASCRIPT/aula16/script.js
@@ -1,62 +1,19 @@
-function verificar() {
-    var  ini = document.getElementById('txtini')
-    var fim = data.getFullYear()
-    var pass = document.getElementById('txtfim')
-    var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value ==ano) {
+function contar() {
+    let  ini = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpas')
+    
 
 
-
-
-
-
-
-
-
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('Erro')
 
-    } else {
-        var fsex = document.getElementsByName('radiosex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >=0 &&idade <=10){
-                //Criança
-                img.setAttribute('src','bebehomem.png')
-            }else if (idade < 21){
-                //Jovem
-                img.setAttribute('src','jovemhomem.png')
-            }else if(idade <50){
-                //adulto
-                img.setAttribute('src','homemmaduro.png')
-            }else{
-                //idoso
-                img.setAttribute('src','homemvelho.png')
-            }
-
-         }else if (fsex[1].checked) {
-                genero = 'Mulher'
-                if (idade >=0 &&idade <= 10){
-                    //Criança
-                    img.setAttribute('src','bebmulher.png')
-                }else if (idade < 21){
-                    //Jovem
-                    img.setAttribute('src','jovemmulher.png')
-                }else if(idade <50){
-                    //adulto
-                    img.setAttribute('src','mulhermadura.png')
-                }else{
-                    //idoso
-                    img.setAttribute('src','mulhervelha.png')
-                }
-            }
-
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
+    }else{
+            window.alert('Tudo ok')
+        }
     }
-}
+
+        
+
+    
+
