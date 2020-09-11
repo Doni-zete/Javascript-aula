040a0bab4af23fdf1e962a3e78c187182b40edb0
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-11 16:26:41 -0300

    atu

--- JAVASCRIPT/aula16/script.js
+++ JAVASCRIPT/aula16/script.js
@@ -2,6 +2,7 @@ function contar() {
     let  ini = document.getElementById('txtini')
     let fim = document.getElementById('txtfim')
     let passo = document.getElementById('txtpas')
+    let res = document.getElementById('res')
     
 
 
@@ -9,8 +10,17 @@ function contar() {
         window.alert('Erro')
 
     }else{
-            window.alert('Tudo ok')
+            res.innerHTML='Contando... '
+            let i = Number(ini.value)
+            let f = Number(fim.value)
+            let p = Number(passo.value)
+
+            for(let c =i; c <=f; c += p){
+                res.innerHTML +=`${c} \u{1F449} `
+            }
+            res.innerHTML +=` \u{1F3C1}`
         }
+       
     }
 
         
