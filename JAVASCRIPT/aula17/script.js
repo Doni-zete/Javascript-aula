4e2190c8af8908f19afe64d1ee1fe3b67b6d032a
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-11 19:14:35 -0300

    tabuada

--- JAVASCRIPT/aula17/script.js
+++ JAVASCRIPT/aula17/script.js
@@ -1,34 +1,19 @@
-function contar() {
-    let ini = document.getElementById('txtini')
-    let fim = document.getElementById('txtfim')
-    let passo = document.getElementById('txtpas')
-    let res = document.getElementById('res')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
 
-
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossivel contar!'
-        window.alert('Erro')
-        
-        
-
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
     } else {
-        res.innerHTML = 'Contando: '
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (i < f) {
-
-            for (let c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{1F449} `
-            }
-
-        } else {
-            for (let c = i; c >= f; c -= p) {
-                res.innerHTML += ` ${c} \u{1F449} `
-            }
+        let n = Number(num.value)
+        let c =1
+        while (c <= 10){
+            let item = document.getElementById
         }
-        res.innerHTML += ` \u{1F3C1}`
+
+
+
     }
+}
 }
 
