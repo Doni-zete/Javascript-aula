34d61653769370b4a7c08f28bf5a769649dde972
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-11 11:47:51 -0300

    Update script.js

--- JAVASCRIPT/aula16/script.js
+++ JAVASCRIPT/aula16/script.js
@@ -1,9 +1,18 @@
 function verificar() {
-    var data = new Date()
+    var  ini = document.getElementById('')
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || fano.value ==ano) {
+
+
+
+
+
+
+
+        
+
         window.alert('Erro')
 
     } else {
