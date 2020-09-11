0e48f4a4c1cb8f598d7980d9e841205f54ccc76a
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-11 18:16:26 -0300

    aat

--- JAVASCRIPT/aula16/script.js
+++ JAVASCRIPT/aula16/script.js
@@ -1,29 +1,34 @@
 function contar() {
-    let  ini = document.getElementById('txtini')
+    let ini = document.getElementById('txtini')
     let fim = document.getElementById('txtfim')
     let passo = document.getElementById('txtpas')
     let res = document.getElementById('res')
-    
+
 
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
         window.alert('Erro')
-
-    }else{
-            res.innerHTML='Contando... '
-            let i = Number(ini.value)
-            let f = Number(fim.value)
-            let p = Number(passo.value)
-
-            for(let c =i; c <=f; c += p){
-                res.innerHTML +=`${c} \u{1F449} `
-            }
-            res.innerHTML +=` \u{1F3C1}`
-        }
-       
-    }
-
+        
         
 
-    
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
+    }
+}
 
