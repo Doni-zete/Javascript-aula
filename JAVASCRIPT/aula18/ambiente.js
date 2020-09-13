2ea476e93dbc4a9b800ec7168e371fdb142b7b56
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-13 16:23:32 -0300

    funcao

--- JAVASCRIPT/aula18/ambiente.js
+++ JAVASCRIPT/aula18/ambiente.js
@@ -1,6 +1,15 @@
-let num = [5,8,2,9,3,6]
-num.sort()
-num.push(1)
-console.log(num)
-console.log(`O valor tem ${num.length} posições`)
-console.log(`${num[3]}`)
+let valores = [8,1,7,4,2,9]
+
+
+
+/*
+for (let pos=0; pos <= valores.lenght; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores [pos]}`)
+}
+*/
+
+
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`) 
+
+} 
