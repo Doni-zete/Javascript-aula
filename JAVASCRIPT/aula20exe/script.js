cdf2ff57f434890fb2dc1a7f1c4ec16201c2a891
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-14 09:05:23 -0300

    finalizado

--- JAVASCRIPT/aula20exe/script.js
+++ JAVASCRIPT/aula20exe/script.js
@@ -29,21 +29,38 @@ function analisa() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
+        res.innerHTML=''
     } else {
         window.alert('Valor invalido ou ja encontrado na lista')
     }
     num.value = ''
     num.focus()
-
+}
     function finalizar() {
         if (valores.length == 0){
             window.alert('Adicione valores antes de finalizar!')
 
         }else{
             let tot = valores.length
+            let maior =valores[0]
+            let menor =valores [0]
+            let soma = 0
+            let media = 0
 
+            for (let pos in valores){
+                soma += valores[pos]
+                media += valores[pos]
+                if(valores[pos]> maior)
+                maior=valores[pos]
+                if(valores[pos]<menor)
+                menor=valores[pos]
+            }
+            media = soma/tot
             res.innerHTML =''
-            res.innerHTML  +=`<p> Ao todo,temos ${tot}numeros cadastrados.</p>`
+            res.innerHTML  +=`<p> Ao todo, temos ${tot} numeros cadastrados.</p>`
+            res.innerHTML +=`<p> O maior valor é: ${maior}</p>`
+            res.innerHTML +=`<p> O menor valor é: ${menor}</p>`
+            res.innerHTML +=`<p> A soma dos numero é: ${soma}</p>`
+            res.innerHTML+=`<p> A media é: ${media}</p> `
         }
     }
-}
